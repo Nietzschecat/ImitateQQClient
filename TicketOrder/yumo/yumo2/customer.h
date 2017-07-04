341b3e5c84b5f5f643de5a4cf314555cc0e88193
yumo <737021031@qq.com>
2017-07-04 16:49:29 +0800

    first commit

---
+++ TicketOrder/yumo/yumo2/customer.h
@@ -0,0 +1,11 @@
+namespace yumo2 {
+	using namespace System;
+
+	public ref class Customer {
+	public:
+		String^ m_CompnayName;
+	    String^ m_IDNumber;
+		String^ m_Time;
+		Customer() { };
+	};
+}
---
+++ TicketOrder/yumo/yumo2/manager.h
@@ -0,0 +1,12 @@
+namespace yumo2 {
+	using namespace System;
+
+	public ref class Manager {
+	public:
+		String^ n_CompnayName;
+		String^ n_StartPlace;
+		String^ n_Terminal;
+		String^ n_Time;
+		Manager() { };
+	};
+}
